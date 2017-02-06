b169423ab78ff812958ea3c5ac11d540e78e703f
enigmaneo <enigmaneo@users.noreply.github.com>
2017-02-05 23:57:36 -0500

    add parentheses to about page

--- src/components/about/AboutPage.js
+++ src/components/about/AboutPage.js
@@ -5,7 +5,7 @@ class AboutPage extends React.Component {
         return (
             <div>
                 <h1>About</h1>
-                <p>This app was created by Richard Montgomery (<a href="mailto:rmontgo@gmail.com">rmontgo@gmail.com</a></p>
+                <p>This app was created by Richard Montgomery (<a href="mailto:rmontgo@gmail.com">rmontgo@gmail.com</a>)</p>
             </div>
         );
     }
